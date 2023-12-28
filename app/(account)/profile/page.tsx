8cd030d3098a089efb1f99fd4223b691b99e8b57
Aryan Vishwakarma <aryanongithub@gmail.com>
2023-12-28 19:39:50 +0530

    fix(profile): profileImage undefined error attemp - 2

--- app/(account)/profile/page.tsx
+++ app/(account)/profile/page.tsx
@@ -11,7 +11,7 @@ async function page({ searchParams }: { searchParams: { [key: string]: string } 
   return (
     <section className="py-[8vw]">
       <div className="flexCenter flex-col gap-2 ">
-        <Image src={data.data?.profileImage || ""}  className='rounded-full aspect-square object-cover' alt="profileImageUser" width={160} height={160} />
+        <Image src={data.data.profileImage || "/brands/pintrest.png"}  className='rounded-full aspect-square object-cover' alt="profileImageUser" width={160} height={160} />
         <h1 className='text-3xl font-semibold'> {data.data.name} </h1>
         <p className="flexCenter gap-2">
           <Image src="/brands/pintrest.png" alt="logo " width={20} height={20} className="grayscale" />
@@ -19,7 +19,7 @@ async function page({ searchParams }: { searchParams: { [key: string]: string } 
             data.data.username
           }
         </p>
-        <p className='max-w-sm text-center'> <Link href={data.data && data.data.website || "/"} className='font-semibold' target="_blank"> {url && url.hostname} </Link>  {data.data?.about} </p>
+        <p className='max-w-sm text-center'> <Link href={data.data && data.data?.website || "/"} className='font-semibold' target="_blank"> {url && url.hostname} </Link>  {data.data?.about} </p>
         <Link href="/profile/edit" className="bg-red-600 hover:opacity-80 px-4 py-2 text-white rounded-full "> Edit Profile </Link>
         <div className='flex items-center gap-5 my-7'>
           <Link href={`/profile?topic=created`} className={` px-2 py-1 rounded-full ${searchParams.topic === 'created' ? 'bg-red-600 text-white '  : 'border border-red-500 text-red-500'}`} >Created</Link>
